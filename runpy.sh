e22c5a5cf1c9387c6e596080ca5ed6f4517c4b0c
TamNguyenZ <thanhnhan211210@gmail.com>
2025-11-20 10:22:02 +0700

    Update runpy.sh

--- runpy.sh
+++ runpy.sh
@@ -1,1 +1,1 @@
-export PATH="$HOME/python3.12/bin:$PATH" && export LD_LIBRARY_PATH="$HOME/python3.12/lib:$LD_LIBRARY_PATH" && "$HOME/python3.12/bin/python3.12" win.py
+export PATH="$HOME/python3.10/bin:$PATH" && export LD_LIBRARY_PATH="$HOME/python3.10/lib:$LD_LIBRARY_PATH" && "$HOME/python3.10/bin/python3.10" win.py
